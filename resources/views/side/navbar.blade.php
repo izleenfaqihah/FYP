e2cd0f5d84652cc138e3fa0045a4c13ddc551b55
izleenfaqihah <izleenfaqihah@gmail.com>
2019-03-03 15:39:15 +0800

    first commit

---
+++ resources/views/side/navbar.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>  
+</head>
+<style>
+	
+</style>
+<body>
+	
+	<div class="container-fluid">
+        <div class="row">
+            <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
+            	
+				  <a class="nav-link" href="{{ route('home') }}">Dashboard</a>
+				  <a class="nav-link" href="{{ route('project') }}">Project</a>
+				  <a class="nav-link" href="{{ route('task') }}">Task</a>
+				  <a class="nav-link" href="{{ route('analytic') }}">Analytic</a>
+
+			</nav>
+        </div>
+    </div>
+</body>
+</html>
